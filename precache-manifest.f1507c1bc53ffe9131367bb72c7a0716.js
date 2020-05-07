243c0b0de4f2a63384559c3b3d41d06013be2a92
Christian Septimo <clowreed@gmail.com>
2020-05-07 19:43:22 +0800

    Updates

---
+++ precache-manifest.f1507c1bc53ffe9131367bb72c7a0716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac2ee67b584b0bc6483fbdc4076460d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0fca724a3e5b753149a2",
+    "url": "/monsters-rolodex/static/css/main.3bdd166c.chunk.css"
+  },
+  {
+    "revision": "e8743bf758ecf001cdf3",
+    "url": "/monsters-rolodex/static/js/2.2a6eed2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2a6eed2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fca724a3e5b753149a2",
+    "url": "/monsters-rolodex/static/js/main.8aaebb78.chunk.js"
+  },
+  {
+    "revision": "68b00582aea0f61137cd",
+    "url": "/monsters-rolodex/static/js/runtime-main.4c13341c.js"
+  }
+]);
